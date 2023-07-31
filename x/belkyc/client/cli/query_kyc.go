15c7e58f88ffd3394e4e4ab0748ddadde2472cd9
anshulWeb3 <oitw_chm@tutanota.com>
2023-07-31 13:19:30 +0530

    kyc mapping added

---
+++ x/belkyc/client/cli/query_kyc.go
@@ -0,0 +1,78 @@
+package cli
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+
+	"belkyc/x/belkyc/types"
+)
+
+func CmdListKyc() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-kyc",
+		Short: "list all kyc",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryAllKycRequest{
+				Pagination: pageReq,
+			}
+
+			res, err := queryClient.KycAll(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddPaginationFlagsToCmd(cmd, cmd.Use)
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdShowKyc() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show-kyc [address]",
+		Short: "shows a kyc",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			argAddress := args[0]
+
+			params := &types.QueryGetKycRequest{
+				Address: argAddress,
+			}
+
+			res, err := queryClient.Kyc(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
--- x/belkyc/module_simulation.go
+++ x/belkyc/module_simulation.go
@@ -23,7 +23,19 @@ var (
 )
 
 const (
-// this line is used by starport scaffolding # simapp/module/const
+	opWeightMsgCreateKyc = "op_weight_msg_kyc"
+	// TODO: Determine the simulation weight value
+	defaultWeightMsgCreateKyc int = 100
+
+	opWeightMsgUpdateKyc = "op_weight_msg_kyc"
+	// TODO: Determine the simulation weight value
+	defaultWeightMsgUpdateKyc int = 100
+
+	opWeightMsgDeleteKyc = "op_weight_msg_kyc"
+	// TODO: Determine the simulation weight value
+	defaultWeightMsgDeleteKyc int = 100
+
+	// this line is used by starport scaffolding # simapp/module/const
 )
 
 // GenerateGenesisState creates a randomized GenState of the module.
@@ -34,6 +46,16 @@ func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
 	}
 	belkycGenesis := types.GenesisState{
 		Params: types.DefaultParams(),
+		KycList: []types.Kyc{
+			{
+				Creator: sample.AccAddress(),
+				Address: "0",
+			},
+			{
+				Creator: sample.AccAddress(),
+				Address: "1",
+			},
+		},
 		// this line is used by starport scaffolding # simapp/module/genesisState
 	}
 	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(&belkycGenesis)
@@ -51,6 +73,39 @@ func (AppModule) ProposalContents(_ module.SimulationState) []simtypes.WeightedP
 func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
 	operations := make([]simtypes.WeightedOperation, 0)
 
+	var weightMsgCreateKyc int
+	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgCreateKyc, &weightMsgCreateKyc, nil,
+		func(_ *rand.Rand) {
+			weightMsgCreateKyc = defaultWeightMsgCreateKyc
+		},
+	)
+	operations = append(operations, simulation.NewWeightedOperation(
+		weightMsgCreateKyc,
+		belkycsimulation.SimulateMsgCreateKyc(am.accountKeeper, am.bankKeeper, am.keeper),
+	))
+
+	var weightMsgUpdateKyc int
+	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgUpdateKyc, &weightMsgUpdateKyc, nil,
+		func(_ *rand.Rand) {
+			weightMsgUpdateKyc = defaultWeightMsgUpdateKyc
+		},
+	)
+	operations = append(operations, simulation.NewWeightedOperation(
+		weightMsgUpdateKyc,
+		belkycsimulation.SimulateMsgUpdateKyc(am.accountKeeper, am.bankKeeper, am.keeper),
+	))
+
+	var weightMsgDeleteKyc int
+	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgDeleteKyc, &weightMsgDeleteKyc, nil,
+		func(_ *rand.Rand) {
+			weightMsgDeleteKyc = defaultWeightMsgDeleteKyc
+		},
+	)
+	operations = append(operations, simulation.NewWeightedOperation(
+		weightMsgDeleteKyc,
+		belkycsimulation.SimulateMsgDeleteKyc(am.accountKeeper, am.bankKeeper, am.keeper),
+	))
+
 	// this line is used by starport scaffolding # simapp/module/operation
 
 	return operations
@@ -59,6 +114,30 @@ func (am AppModule) WeightedOperations(simState module.SimulationState) []simtyp
 // ProposalMsgs returns msgs used for governance proposals for simulations.
 func (am AppModule) ProposalMsgs(simState module.SimulationState) []simtypes.WeightedProposalMsg {
 	return []simtypes.WeightedProposalMsg{
+		simulation.NewWeightedProposalMsg(
+			opWeightMsgCreateKyc,
+			defaultWeightMsgCreateKyc,
+			func(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) sdk.Msg {
+				belkycsimulation.SimulateMsgCreateKyc(am.accountKeeper, am.bankKeeper, am.keeper)
+				return nil
+			},
+		),
+		simulation.NewWeightedProposalMsg(
+			opWeightMsgUpdateKyc,
+			defaultWeightMsgUpdateKyc,
+			func(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) sdk.Msg {
+				belkycsimulation.SimulateMsgUpdateKyc(am.accountKeeper, am.bankKeeper, am.keeper)
+				return nil
+			},
+		),
+		simulation.NewWeightedProposalMsg(
+			opWeightMsgDeleteKyc,
+			defaultWeightMsgDeleteKyc,
+			func(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) sdk.Msg {
+				belkycsimulation.SimulateMsgDeleteKyc(am.accountKeeper, am.bankKeeper, am.keeper)
+				return nil
+			},
+		),
 		// this line is used by starport scaffolding # simapp/module/OpMsg
 	}
 }
